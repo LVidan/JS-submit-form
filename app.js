1488a3ae53b058d30932411a22fc36ef4cb6fd90
Vidan Lugonja <vidan.lugonja@gmail.com>
2020-09-09 00:11:35 +0200

    vl-09-09-2020 - adding comment to the localStorage and getting comment from the localStorage

---
+++ app.js
@@ -0,0 +1,50 @@
+const submitBtn = document.querySelector('.comment-submit');
+const commentList = document.querySelector('.comments');
+const commentInput = document.querySelector('.comment-input');
+
+
+submitBtn.addEventListener('click', appendComment, false);
+
+function appendComment(event) {
+	const data = {
+		avatar: 'https://secure.gravatar.com/avatar/d1f5ca0d7e625f334c5186e112b77ebd',
+		comment: commentInput.value,
+		author: 'Philip Demo'
+	}
+	
+	if (commentInput.value === '') {
+		event.preventDefault();
+		return;
+	}
+
+	event.preventDefault();
+	template(data);
+	commentInput.value = '';
+
+	// Save to the localStorage
+	localStorage.setItem('CommentList', commentList.innerHTML);
+}
+
+function template(data) {
+	commentList.insertAdjacentHTML("beforeend", `
+		<div class="comment flex items-start justify-start">
+			<img
+				class="comment-avatar"
+				src="${data.avatar}"
+				alt="${data.author}"
+			/>
+			<div class="flex-1">
+				<h3 class="comment-author">${data.author}</h3>
+				<p class="comment-body">${data.comment}</p>
+			</div>
+		</div>
+	`);
+}
+
+
+// store data
+const saved = localStorage.getItem('CommentList');
+
+if (saved !== null) {
+	commentList.innerHTML = saved;
+}
